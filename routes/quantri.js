634d4e11a9fbac41b50ff1cb90b80702d8f2c8cb
buckblack <42344854+buckblack@users.noreply.github.com>
2019-04-15 17:24:29 +0700

    .

--- routes/quantri.js
+++ routes/quantri.js
@@ -6,7 +6,14 @@ var md5=require('md5');
 cl_nguoi_dung = 'nguoi_dung'
 
 router.get('/', function(req, res, next) {
-  res.render('quan_tri/nhan_vien', {});
+  res.redirect('/quantri/nhan-vien');
+});
+
+router.get('/nhan-vien',async function(req, res, next) {
+  let db = await xl_mongo.Get();
+  await db.collection(cl_nguoi_dung).find({'chuc_vu':'nhân viên'}).toArray((err,result)=>{
+    res.render('quan_tri/nhan_vien', {nhan_vien:result});
+  })
 });
 
 router.post('/dang-nhap',async function(req, res, next) {
@@ -35,6 +42,7 @@ router.post('/dang-nhap',async function(req, res, next) {
           'qt_Email': result[0].email,
           'qt_Dia_chi': result[0].dia_chi,
           'qt_Dien_thoai': result[0].dien_thoai,
+          'qt_Gioi_tinh': result[0].gioi_tinh,
         }
         res.json(JSON.stringify(kq))
       }
@@ -48,13 +56,10 @@ router.get('/dang-nhap', function(req, res, next) {
   res.render('quan_tri/dang_nhap', {});
 });
 
-router.get('/dangky', function(req, res, next) {
-  res.render('dangky', { tieude: 'Gear Srore | Đăng ký',trangthai:"Đăng ký" });
-});
 
-router.post('/dangky',async function(req, res, next) {
+router.post('/them-nhan-vien',async function(req, res, next) {
   let db = await xl_mongo.Get();
-  var dk={'email':req.body.email,'chuc_vu':'khách hàng'}
+  var dk={'email':req.body.email,'chuc_vu':'nhân viên'}
   await db.collection(cl_nguoi_dung).find(dk).toArray((err,result)=>{
     if(result.length>0)
     {
@@ -68,24 +73,15 @@ router.post('/dangky',async function(req, res, next) {
         'ho_ten' : req.body.ho_ten,
         'dia_chi' : req.body.dia_chi,
         'password' : md5(req.body.password),
-        'chuc_vu' : req.body.chuc_vu,
+        'chuc_vu' : 'nhân viên',
         'dien_thoai' : req.body.dien_thoai,
         'gioi_tinh' : req.body.gioi_tinh,
-        'trang_thai' : 'đang hoạt động'
+        'trang_thai' : req.body.trang_thai
       }
       db.collection(cl_nguoi_dung).insert(user,(loi,ket_qua)=>{
-        var from = "long4581994@gmail.com"
-        var to = req.body.email
-        var subject = "Đăng ký tài khoản thành công"
-        var body = `Chào ${req.body.ho_ten}. Cảm ơn bạn đã đăng ký làm thành viên của Gear Store`
-        var kqPromise = Gui_thu.Gioi_Thu_Lien_he(from, to, subject, body)
-        kqPromise.then(r => {
-        }).catch(err => {
-          console.log(err);
-        })
         var kq={
           'errorCode':0,
-          'message':'Đăng ký thành công',
+          'message':'Thêm thành công',
         }
         res.json(JSON.stringify(kq))
       })
@@ -94,46 +90,22 @@ router.post('/dangky',async function(req, res, next) {
   })
 });
 
-
-
-router.post('/doimatkhau',async function(req, res, next) {
+router.post('/cap-nhat',async function(req, res, next) {
   let db = await xl_mongo.Get();
-  var dk={'_id':ObjectId(req.body.id),'password':md5(req.body.mat_khau_hien_tai)}
-  await db.collection(cl_nguoi_dung).find(dk).toArray((err,result)=>{
-    if(result.length!=1)
-    {
-      var kq={'errorCode':1, 'message':'Mật khẩu hiện tại không đúng'}
-      res.json(JSON.stringify(kq))
-    }
-    else
-    {
-      if(result[0].trang_thai=='khóa')
-      {
-        var kq={'errorCode':2, 'message':'Tài khoản đã bị khóa'}
-        res.json(JSON.stringify(kq))
-      }
-      else
-      {
-        db.collection(cl_nguoi_dung).update({'_id':ObjectId(req.body.id)},{$set:{'password':md5(req.body.mat_khau_moi)}},(loi,ket_qua)=>{
-          var kq={
-            'errorCode':0,
-            'message':'Đổi mật khẩu thành công',
-          }
-          res.json(JSON.stringify(kq))
-        })
-      }
-      
-    }
-    
+  db.collection(cl_nguoi_dung).update({'_id':ObjectId(req.body.id)},{$set:{
+    'ho_ten':req.body.ho_ten,
+    'dia_chi':req.body.dia_chi,
+    'dien_thoai':req.body.dien_thoai,
+    'trang_thai':req.body.trang_thai,
+    'gioi_tinh':req.body.gioi_tinh
+  }},()=>{
+    res.json({
+      'message':'Cập nhật thành công',
+      'errorCode':0
+    })
   })
 });
 
-/*router.post('/dangnhap', function(req, res, next) {
-  if(req.body.ten_dang_nhap=="admin")
-    res.render('dangnhap', { tieude: 'Đăng nhập',trangthai:"post" });
-  else {
-    res.render('dangky', { tieude: 'Đăng nhập',trangthai:"post" });
-  }
-});*/
+
 
 module.exports = router;
